bcd2aa0200ba75450d27b43c9737c2d3f9285554
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2023-03-06 21:58:26 +0700

    Reading request headers and setting response headers

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
-  res.end('Hello World');
+  var body = 'Hello World';
+  res.setHeader('Content-Length', body.length);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(body);
 });
 server.listen(3000);
