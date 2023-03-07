49aa1bd05e488e511669edf3abd541aed3007ab6
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2023-03-07 22:20:27 +0700

    rest method

--- server.js
+++ server.js
@@ -14,5 +14,25 @@ var server = http.createServer(function (req, res) {
         res.end('OK\n');
       });
       break;
+    case 'GET':
+      items.forEach(function (item, i) {
+        res.write(i + ') ' + item + '\n');
+      });
+      res.end();
+      break;
+    case 'DELETE':
+      var path = url.parse(req.url).pathname;
+      var i = parseInt(path.slice(1), 10);
+      if (isNaN(i)) {
+        res.statusCode = 400;
+        res.end('Invalid item id');
+      } else if (!items[i]) {
+        res.statusCode = 404;
+        res.end('Item not found');
+      } else {
+        items.splice(i, 1);
+        res.end('OK\n');
+      }
+      break;
   }
 });
