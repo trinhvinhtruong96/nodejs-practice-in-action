86f24e4731b064cbdf3fe0750047bf4c6c1bd4a3
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2023-03-07 22:21:01 +0700

    Serving static files

--- server.js
+++ server.js
@@ -1,38 +1,17 @@
 var http = require('http');
-var url = require('url');
-var items = [];
+var parse = require('url').parse;
+var join = require('path').join;
+var fs = require('fs');
+var root = __dirname;
 var server = http.createServer(function (req, res) {
-  switch (req.method) {
-    case 'POST':
-      var item = '';
-      req.setEncoding('utf8');
-      req.on('data', function (chunk) {
-        item += chunk;
-      });
-      req.on('end', function () {
-        items.push(item);
-        res.end('OK\n');
-      });
-      break;
-    case 'GET':
-      items.forEach(function (item, i) {
-        res.write(i + ') ' + item + '\n');
-      });
-      res.end();
-      break;
-    case 'DELETE':
-      var path = url.parse(req.url).pathname;
-      var i = parseInt(path.slice(1), 10);
-      if (isNaN(i)) {
-        res.statusCode = 400;
-        res.end('Invalid item id');
-      } else if (!items[i]) {
-        res.statusCode = 404;
-        res.end('Item not found');
-      } else {
-        items.splice(i, 1);
-        res.end('OK\n');
-      }
-      break;
-  }
+  var url = parse(req.url);
+  var path = join(root, url.pathname);
+  var stream = fs.createReadStream(path);
+  stream.on('data', function (chunk) {
+    res.write(chunk);
+  });
+  stream.on('end', function () {
+    res.end();
+  });
 });
+server.listen(3000);
