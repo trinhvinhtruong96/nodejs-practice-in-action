4f14e539111fde84d349326999231e09ecbc27b5
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2023-03-08 22:22:36 +0700

    Handling server errors

--- server.js
+++ server.js
@@ -22,4 +22,8 @@ var server = http.createServer(function (req, res) {
   var path = join(root, url.pathname);
   var stream = fs.createReadStream(path);
   stream.pipe(res);
+  stream.on('error', function (err) {
+    res.statusCode = 500;
+    res.end('Internal Server Error');
+  });
 });
