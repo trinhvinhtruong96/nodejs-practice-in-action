71447ca1a41b1ee65114918cb687a4d133d7ed0d
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2023-03-19 15:09:26 +0700

    build in middleware

---
+++ examples/buildInMiddleware.js
@@ -0,0 +1,5 @@
+//  cookieParser()—Parses cookies from web browsers into req.cookies
+//  bodyParser()—Consumes and parses the request body into req.body
+//  limit()—Goes hand in hand with bodyParser() to keep requests from getting
+// too big
+//  query()—Parses the request URL query string into req.query
